5f2de6510fcb57012a1addd1a3db0fc4794ac953
edd <edwinkamaumuraya0@outlook.com>
2018-05-18 09:25:52 +0300

    added some class files

--- TheBasicStructureofaConsole/TheBasicStructureofaConsole/Program.cs
+++ TheBasicStructureofaConsole/TheBasicStructureofaConsole/Program.cs
@@ -2,13 +2,43 @@
 
 namespace TheBasicStructureofaConsole
 {
+    /// <summary>
+    /// different main method signature
+    /// </summary>
     class Program
     {
         //Todo Create a console application using dotnetcore and dotnet standard
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             //Output method call
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("****This is my First C# application");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ForegroundColor=
+            Console.WriteLine("Please Enter your Name");
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Please Enter your name");
+
+            Console.WriteLine(Console.ReadLine());
+            Console.ReadKey();
+            return 0;
         }
+        /*
+        //no parameters and a return type
+        static void Main()
+        {
+
+        }
+
+        //int return type, no parameters
+        static int Main()
+        {
+            return 0;
+        }
+        */
     }
 }
---
+++ TheBasicStructureofaConsole/TheBasicStructureofaConsole/StructureoFConsoleApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TheBasicStructureofaConsole
+{
+    /// <summary>
+    /// the basic Structure of A console application
+    /// </summary>
+    class StructureoFConsoleApp
+    {
+        // static void Main()
+        //{
+        //    Console.WriteLine("Hello world");
+        //    Console.WriteLine("This is my first C# project");
+
+        //    Console.ReadKey();
+        //}
+        
+    }
+}
