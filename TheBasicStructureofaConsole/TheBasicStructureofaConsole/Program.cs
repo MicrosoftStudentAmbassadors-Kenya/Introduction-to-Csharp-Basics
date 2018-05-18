b968e33eabc2a40b26f10b48c747137a89fe6071
edd <edwinkamaumuraya0@outlook.com>
2018-05-18 08:45:09 +0300

    added a readme file

--- TheBasicStructureofaConsole/TheBasicStructureofaConsole/Program.cs
+++ TheBasicStructureofaConsole/TheBasicStructureofaConsole/Program.cs
@@ -4,8 +4,10 @@ namespace TheBasicStructureofaConsole
 {
     class Program
     {
+        //Todo Create a console application using dotnetcore and dotnet standard
         static void Main(string[] args)
         {
+            //Output method call
             Console.WriteLine("Hello World!");
         }
     }
