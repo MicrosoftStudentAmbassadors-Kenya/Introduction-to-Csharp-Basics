66e0e105fdb7b599a90a08d7924ddb386b71b054
Jcardif <josh.cardif14@outlook.com>
2018-05-20 09:19:43 +0300

    Initialise C# operators

---
+++ Csharp  Operators/Csharp  Operators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp__Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
