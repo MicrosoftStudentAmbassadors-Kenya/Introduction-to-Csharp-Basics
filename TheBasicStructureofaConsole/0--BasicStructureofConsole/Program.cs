43c8d364ac38a845c169a3ed7dec15e766f9ed4d
edd <edwinkamaumuraya0@outlook.com>
2018-05-27 20:29:52 +0300

    added  a console application project

---
+++ TheBasicStructureofaConsole/0--BasicStructureofConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _0__BasicStructureofConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
+       
