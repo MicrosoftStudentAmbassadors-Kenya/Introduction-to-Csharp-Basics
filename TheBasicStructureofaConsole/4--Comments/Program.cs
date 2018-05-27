656ec2f37da49dfe48f5f9661bcf6baf7429a0ab
edd <edwinkamaumuraya0@outlook.com>
2018-05-27 20:50:48 +0300

    added a comment to the project file

---
+++ TheBasicStructureofaConsole/4--Comments/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4__Comments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
