62d3b53b8da3bc7d15f60736fee88ff5bcc8a6e3
edd <edwinkamaumuraya0@outlook.com>
2018-05-27 21:06:33 +0300

    made final changes of the project prep

--- TheBasicStructureofaConsole/0--BasicStructureofConsole/Program.cs
+++ TheBasicStructureofaConsole/0--BasicStructureofConsole/Program.cs
@@ -4,8 +4,19 @@ namespace _0__BasicStructureofConsole
 {
     class Program
     {
+        /// <summary>
+        /// rules of C# programming language
+        ///
+        /// C# is case sensitive
+        /// All statements must end with a semicolon
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+
+
+            //Outputs text to the screen ot the console
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
