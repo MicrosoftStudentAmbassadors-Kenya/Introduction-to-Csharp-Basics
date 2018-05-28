ecd1e09ae6e18746751ed14cbaf5438d6d78fc5c
Jcardif <josh.cardif14@outlook.com>
2018-05-29 01:08:08 +0300

    One

--- Csharp  Operators/Csharp  Operators/Program.cs
+++ Csharp  Operators/Csharp  Operators/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Transactions;
 
 namespace Csharp__Operators
 {
@@ -6,7 +7,25 @@ namespace Csharp__Operators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Area of a rectangle
+            Console.Title = "Area of Rectangle";
+            int length, width, area;
+
+            Console.WriteLine("Enter The length");
+            //convert the string into an integer
+            length = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter The width");
+            //convert the string into an integer
+            width = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            area = length * width;
+            Console.WriteLine($"The area of the Rectangle is {area}cm^2");
+            Console.ReadKey();
+
+            //Area 
+            
         }
     }
 }
