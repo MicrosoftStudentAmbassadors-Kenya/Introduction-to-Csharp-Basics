8b41185f197906da3dd28eca7029c2e6cffe38d4
Jcardif <josh.cardif14@outlook.com>
2018-05-29 07:24:36 +0300

    Complete example 2

--- Csharp  Operators/Csharp  Operators/Program.cs
+++ Csharp  Operators/Csharp  Operators/Program.cs
@@ -24,8 +24,33 @@ namespace Csharp__Operators
             Console.WriteLine($"The area of the Rectangle is {area}cm^2");
             Console.ReadKey();
 
-            //Area 
-            
+            //Area of inner width in both cm and metres given tha the area of path is 450.575m^2  (ans  11.56m)
+            //           _____________________________
+            //          |   ___________20.65m______   |
+            //          |  |                       |  |
+            //          |  |                       |  |
+            //          |  Y                       |  18.24m
+            //          |  |                       |  |
+            //          |  |                       |  |
+            //          |  |_______________________|  |
+            //          |____________37.79m___________|
+
+            Console.Title = "Area of path";
+            double largeArea, smallArea, remainder, y;
+
+            //Calculate the areas
+            largeArea = 37 * 18;
+            smallArea = 18.24 * 37.79 - 450.575;
+
+            //compute the width in cm
+            y = (smallArea / 20.65)*100;
+
+            //compute the measurement in both cm and m
+            var wholeNo = (y / 100);
+            remainder = y % 100;
+
+            //Answer
+            Console.WriteLine($"{wholeNo:N0}m {remainder:N0}cm");
         }
     }
 }
