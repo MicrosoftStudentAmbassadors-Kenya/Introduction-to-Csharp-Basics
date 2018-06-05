2ed16a26fbf6078902dc8531f39dff27a86a74b2
edd <edwinkamaumuraya0@outlook.com>
2018-06-05 07:25:17 +0300

    created a new project for linq tutorials

---
+++ Linqturorial/Linqturorial/DbContextTutorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Linqturorial
+{
+  public  class DbContextTutorial:DbContext
+    {
+        public DbContextTutorial(DbContextOptions<DbContextTutorial> conne):base(conne)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Linqturorial/Linqturorial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Linqturorial
+{
+    class Program
+    {
+        public static IConfiguration configuration;
+        static void Main(string[] args)
+        {
+            string connectionstring = configuration.GetConnectionString("");
+        }
+    }
+}
