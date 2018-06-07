e214b2705aac5b79a8dc9d4cc49111778a1b2b87
edd <edwinkamaumuraya0@outlook.com>
2018-06-07 11:56:32 +0300

    new visual studio code awesomeness

--- Linqturorial/Linqturorial/Program.cs
+++ Linqturorial/Linqturorial/Program.cs
@@ -8,18 +8,25 @@ namespace Linqturorial
         public static IConfiguration configuration;
         static void Main(string[] args)
         {
+            #region InitializationofNewObject
             string connectionstring = configuration.GetConnectionString("");
             var sequenc=new sequence();
             sequenc.startapp();
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             sequenc.DefferedExecutions();
             Console.ForegroundColor = ConsoleColor.DarkYellow;
+            System.Console.WriteLine("Result showing Immidiate execution with linq");
             sequenc.ImmediateExecutions();
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            System.Console.WriteLine("Result local Immidiate execution with linq");
+            
             sequenc.localQueries();
             Console.ForegroundColor = ConsoleColor.DarkGreen;
+            #endregion
+            System.Console.WriteLine("This is awesome for visual studio code");
             sequenc.fluetSyntax();
-
+            System.Console.WriteLine("l am testing the autosave feature and i will not save this line");
+            
             Console.ReadKey();
         }
     }
