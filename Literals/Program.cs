b24aa419ce1eba0a8fd101e13c07156532bf96f0
edd <edwinkamaumuraya0@outlook.com>
2018-06-14 05:38:05 +0300

    Declared some variables and used the number literals

--- Literals/Program.cs
+++ Literals/Program.cs
@@ -6,6 +6,9 @@ namespace Literals
     {
         static void Main(string[] args)
         {
+            float realNumber=12.5f;
+            realNumber=1.25e+1f;
+            float realNum2=12.5;
             Console.WriteLine("Hello World!");
         }
     }
