a5b8581ec8eccfc3a7cb1c2af4b3b528eb304123
edd <edwinkamaumuraya0@outlook.com>
2018-06-19 18:11:46 +0300

    Create a new project to demo the swich

---
+++ moreSwichCase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace moreSwichCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+    
+        }
+    }
+}
