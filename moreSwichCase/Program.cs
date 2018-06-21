b8f9271d90d0c62dfac1e04108473bf396df376b
Edwin <edwinkamaumuray0@outlook.com>
2018-06-21 17:41:59 +0300

    Created the last change

--- moreSwichCase/Program.cs
+++ moreSwichCase/Program.cs
@@ -8,6 +8,9 @@ namespace moreSwichCase
         {
 
             Console.WriteLine("Hello World!");
+            string input=Console.ReadLine()??"edd";
+            input=char.ToUpper(input[0]).ToString()+input.Substring(1,input.Length-1);
+            Console.WriteLine($"the value input is the following {input}");
             Console.ReadKey();
     
         }
