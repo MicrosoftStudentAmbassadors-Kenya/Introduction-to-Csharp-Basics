21aed85e5c0bfdb134f0bc5e8738f8a5ab49a27c
Edwin <edwinkamaumuray0@outlook.com>
2018-06-22 23:28:49 +0300

    Created a new solution on the nullable types

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -6,7 +6,16 @@ namespace WhileLoop
     {
         static void Main(string[] args)
         {
+            var num1=2;
+            //the following code creates an infinit loop
+            while (num1<10) //conditon
+            {
+                Console.WriteLine("i will never end");
+                num1++;
+            }
             Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
         }
     }
 }
