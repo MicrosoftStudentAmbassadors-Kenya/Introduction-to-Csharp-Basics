5c09565567b0ec6fa8e39ab4026d74391cde6cff
Edwin <edwinkamaumuray0@outlook.com>
2018-06-23 15:31:32 +0300

    created a new project to demo the Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
