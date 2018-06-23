b7343dd7b375f04a5c391c5d9d5ecfd24d62de75
Edwin <edwinkamaumuray0@outlook.com>
2018-06-23 21:59:26 +0300

    added a new project for the swich statement

---
+++ 7.1--moreonConditionalsSwitch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace moreSwichCase {
+
+    class Program {
+        ///When is switch case most applicable  and an if statement applicable
+        //When checking for only one  if one value statisfies in the case.
+        //There are may come a case where you are required to check on two codition if they are satisfied
+        static void Main (string[] args) {
+
+            //Console.WriteLine ("Hello World!");
+            // string input = Console.ReadLine ();
+            //makes the first letter uppercase
+            // input = char.ToUpper (input[0]).ToString () + input.Substring (1, input.Length - 1);
+            // Console.WriteLine ($"the value input is the following {input}");
+            Console.WriteLine ("enter you username");
+            string username = Console.ReadLine ();
+            Console.WriteLine ("enter you password");
+            string password = Console.ReadLine ();
+            //check for only one codition
+            switch (username) {
+                case "Edd":
+                    Console.WriteLine ("Hey Modelator!");
+                    break;
+                case "Josh":
+                    Console.WriteLine ("hey admin");
+                    break;
+                default:
+                    Console.WriteLine ("Hey guest");
+                    break;
+            }
+
+            Console.ReadKey ();
+
+        }
+    }
+}
