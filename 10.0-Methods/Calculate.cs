2c082b91303a76c5b17048a82dd501715ffffd84
Edwin <edwinkamaumuray0@outlook.com>
2018-06-25 02:58:10 +0300

    created the calcualate engine of the project

--- 10.0-Methods/Calculate.cs
+++ 10.0-Methods/Calculate.cs
@@ -1,7 +1,41 @@
-﻿namespace Calculate
+﻿using System;
+
+namespace Calculate
 {
-    class CalculateEngine
+   public class CalculateEngine
     {
-        
+        public double Calcuate(string operation, double firstNumber, double secondNumber)
+        {
+            double result;
+            switch (operation)
+            {
+                case "add":
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "subtract":
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "multiply":
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "modulus":
+                case "%":
+                    result = firstNumber % secondNumber;
+                    break; 
+                case "divide":
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                default:
+                   throw new InvalidOperationException("specified operation not recognised");
+                    break;     
+                 
+            }
+
+            return result;
+        }
     }
 }
