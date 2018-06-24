fe0495bc5ceff6cd3ba768e8fb58011341875bcf
Edwin <edwinkamaumuray0@outlook.com>
2018-06-24 14:00:28 +0300

    finished the program

--- 9.2--SimpleCalculator/Program.cs
+++ 9.2--SimpleCalculator/Program.cs
@@ -8,29 +8,77 @@ namespace SimpleCalculator {
 
             //conversion parameters
             int convertedNum1, convertedNum2;
+            bool isNumeric = false;
+            do {
+                
+                Console.WriteLine ("Enter the first Number ");
+                firstNum = Console.ReadLine ();
+                Console.WriteLine ("Enter the second Number");
+                secondNum = Console.ReadLine ();
 
-            Console.WriteLine ("Enter the first Number ");
-            firstNum = Console.ReadLine ();
-            Console.WriteLine ("Enter the second Number");
-            secondNum = Console.ReadLine ();
+                /*
+                convert the input to intergers
+                chouse the conversion type below couse they return true when suceesfull converted
+                return false when the conversions fails..therefore i can apply the check for numeric 
+                easily
+                */
 
-            //convert the input to intergers
-            //chouse the conversion type below couse they return true when suceesfull converted
-            //return false when the conversions fails..therefore i can apply the check for numeric 
-            //easily
+                //comment out  the following two lines to see the output
+                /*
+                bool a=Int32.TryParse(firstNum,out convertedNum1);
+                Console.WriteLine(a);
+                */
 
-            //on comment out  the following two lines to see the output
-            //bool a=Int32.TryParse(firstNum,out convertedNum1);
-            //Console.WriteLine(a);
+                
+            //    isNumeric = !Int32.TryParse (firstNum, out convertedNum1) ? false : true;
+            //    isNumeric=!Int32.TryParse(secondNum,out convertedNum2)?false:true;    
 
-         
-            if (!Int32.TryParse (firstNum, out convertedNum1))
-                Console.WriteLine ($"Please Enter numeric values. You entered {firstNum}");
+                if (!Int32.TryParse (firstNum, out convertedNum1) ) {
+                    Console.WriteLine ($"Please Enter numeric values. You entered {firstNum}");
+                    isNumeric = true;
+                }else{
+                    isNumeric=false;
+                }
 
-            if (Int32.TryParse (secondNum, out convertedNum2))
-                Console.WriteLine ("Please enter a numerical value..You entered {0}", secondNum);
+                if (!Int32.TryParse (secondNum, out convertedNum2)) {
+                    Console.WriteLine ("Please enter a numerical value..You entered {0}", secondNum);
+                    isNumeric = true;
+                }
+            
 
-            Console.WriteLine ("Hello World!");
-        }
+            } while (isNumeric);
+
+            Console.WriteLine ("What operation is required : add (+) ,subtract (-),Multiply (*),Modulus (%)? please indicate beloe");
+            var operation = Console.ReadLine ();
+            /*make sure to make the comaparison Case insensitive for the words
+            i am checking for the two comaparisons the i.e add (+) 
+            i could enter the name add of the symbol +  either of the two,, add or +
+            Remember for if statement we can combine two comparisons..applicable here
+          
+            for swich we compare one value to some possible casses--- could works to.
+            */
+            //you can use this format
+            //  if(operation.ToLower()=="add")
+
+            //but i prefer this..talk more about the methods 
+            //the Equal methods takes in two parameters,,string beign comapared and the comaparison type
+            //StringComparison.OrdinalIgnoreCase does comaparison letter by letter while ignoring the case
+            if (operation.Equals ("add", StringComparison.OrdinalIgnoreCase) || operation.Equals ("+")) {
+                Console.WriteLine ($"The addition of {convertedNum1} and {convertedNum2} is {convertedNum1+convertedNum2}");
+            } else if (operation.Equals ("subtraction", StringComparison.OrdinalIgnoreCase) || operation.Equals ("+")) {
+                Console.WriteLine ($"The subtract of {convertedNum1} and {convertedNum2} is {convertedNum1-convertedNum2}");
+
+            } else if (operation.Equals ("multiply", StringComparison.OrdinalIgnoreCase) || operation.Equals ("+")) {
+                Console.WriteLine ($"The multiplication of {convertedNum1} and {convertedNum2} is {convertedNum1*convertedNum2}");
+
+            } else if (operation.Equals ("modulus", StringComparison.OrdinalIgnoreCase) || operation.Equals ("+")) {
+                Console.WriteLine ($"The modulus of {convertedNum1} and {convertedNum2} is {convertedNum1%convertedNum2}");
+
+            } else {
+                //works like the default clause
+
+            }
+
+            }
     }
 }
