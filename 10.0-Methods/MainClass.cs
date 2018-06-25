a86d9b04b6b192bd17724d31c89f5beb02e85cf0
Edwin <edwinkamaumuray0@outlook.com>
2018-06-25 03:20:05 +0300

    added a unit test project

--- 10.0-Methods/MainClass.cs
+++ 10.0-Methods/MainClass.cs
@@ -3,9 +3,48 @@ namespace Calculate {
 
     class Calculate {
         static void Main (string[] args) {
-            Console.WriteLine ("This is the method class");
-        
-            Console.ReadKey();
+            
+            //dont want the calcualotor to terminate..i will use an infinite loop
+
+            while (true)
+            {
+                string inputNum1,inputNum2;
+                double numOne, numTwo;
+
+                bool isNumeric = false;
+                do
+                {
+                ChangeTextColor(ConsoleColor.DarkMagenta);
+                Console.WriteLine("Please enter the first number");
+                inputNum1 = Console.ReadLine();
+                Console.WriteLine("Please enter the second Nummber");
+                inputNum2 = Console.ReadLine();
+                try
+                {
+                    numOne = convertToDouble.Convert(inputNum1);
+                    numTwo = convertToDouble.Convert(inputNum2);
+                }
+                catch (Exception e)
+                {
+                    isNumeric = true;
+                    Console.WriteLine(e);
+                    throw new Exception("The number must be numeric");
+                }   
+                    
+                } while (isNumeric);
+               
+
+
+
+
+            }
+            
+          
+        }
+
+     static void ChangeTextColor(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
         }
 
         
--- 10.0-Methods/convertToDouble.cs
+++ 10.0-Methods/convertToDouble.cs
@@ -1,7 +1,27 @@
-﻿namespace Calculate
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace Calculate
 {
     public class convertToDouble
     {
+        public static double Convert(string input)
+        {
+            double convertedNum;
+            try
+            {
+                if (!double.TryParse(input, out convertedNum))
+                {
+                    throw new InvalidCastException("The number entered is not numeric");
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("",e);
+            }
+
+            return convertedNum;
+        }
         
     }
 }
