7f4528cd89e11d0ddeb508492ffc5ea049b0e4bd
Edwin <edwinkamaumuray0@outlook.com>
2018-06-25 19:36:56 +0300

    created a new class project

---
+++ BankAccoutClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAccoutClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
